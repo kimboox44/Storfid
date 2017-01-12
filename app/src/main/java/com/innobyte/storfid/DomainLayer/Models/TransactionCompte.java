d3cf99f0ab9aedaf631af99b5ffb0065bdd1738c
kimboox44 <kimboox44@gmail.com>
2017-01-12 12:38:07 +0100

    1st commit

---
+++ app/src/main/java/com/innobyte/storfid/DomainLayer/Models/TransactionCompte.java
@@ -0,0 +1,11 @@
+package com.innobyte.storfid.DomainLayer.Models;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by Kimboo on 12/01/2017.
+ */
+
+public class TransactionCompte extends RealmObject {
+
+}
